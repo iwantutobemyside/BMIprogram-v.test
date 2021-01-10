366223bb26bdce7f188a550c2a84aca20e41994e
iwantutobemyside <77199247+iwantutobemyside@users.noreply.github.com>
2021-01-10 18:07:55 +0700

    Update BMIprogram.py

--- BMIprogram.py
+++ BMIprogram.py
@@ -1,11 +1,11 @@
 weight = int(input("Enter ur weight(kg): "))
-height = int(input("Enter ur height(cm): ")) / 100 # m -> cm.
+height = int(input("Enter ur height(cm): ")) / 100 #cm -> m.
 BMI = (weight/(height ** 2))
 print("BMI = %.2f" %(BMI))
 
 if BMI < 18.50:
     print("คุณอยู่ในเกณฑ์น้ำหนักน้อยหรือผอม")
-if BMI >= 18.50 and BMI <= 22.90:
+elif BMI >= 18.50 and BMI <= 22.90:
     print("คุณอยู่ในเกณฑ์ปกติ")
 elif BMI >= 23.00 and BMI <= 24.90:
     print("คุณอยู่ในเกณฑ์ที่น้ำหนักเกิน")
